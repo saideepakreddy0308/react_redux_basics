ea909d41180a87016e3618a14b06edd28179d56d
Antino Labs <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-22 12:42:07 +0530

    updated product Listing

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,13 +1,24 @@
 // This is going to have list of our products
 
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductListing = () => {
+  // we now fetch the product we have in our redux store
+  // we will use useSelector hook to fetch the products, useSelector hook takes a function as an argument, this function takes state as an argument
+  // we will return the products from this function
+  // useSelector hook returns the products from the redux store
+  // useSelector hook is similar to mapStateToProps function
+  // useSelector hook is used to fetch the data from the redux store
+
+  const products = useSelector((state) => state);
+  console.log(products);
+
   return (
-    <div>
+    <div className="ui grid container">
       <h1>Product Listing</h1>
     </div>
   );
-}
+};
 
 export default ProductListing;
